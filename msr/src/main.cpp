9f201d8a2db8afe1c12f9dec5e0188c2d526ff62
Pablo Gianni <giannipablo@gmail.com>
2021-03-03 21:08:47 -0300

    Fixed tabulation on setup and loop functions

--- msr/src/main.cpp
+++ msr/src/main.cpp
@@ -73,36 +73,36 @@ bool flagAt0020 = false;
 ////////////////////////////////////////////////
 // Setup
 void setup() {
-  // Serial port configuration
-  Serial.begin(115200);
+    // Serial port configuration
+    Serial.begin(115200);
 
-  ////////////////////////////////////////////////
-  // WiFi stuff
-  Serial.printf("Connecting to %s \n", ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-  Serial.printf("\nConnected to %s \n", ssid);
+    ////////////////////////////////////////////////
+    // WiFi stuff
+    Serial.printf("Connecting to %s \n", ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.printf("\nConnected to %s \n", ssid);
 
-  ////////////////////////////////////////////////
-  // TimeStamp stuff
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  Serial.printf("%s\n",getTimeStamp());
+    ////////////////////////////////////////////////
+    // TimeStamp stuff
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    Serial.printf("%s\n",getTimeStamp());
 
 
-  ////////////////////////////////////////////////
-  // Timer stuff
-  timer = timerBegin(0, 80, true);
-  timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 100000, true);
-  timerAlarmEnable(timer);
+    ////////////////////////////////////////////////
+    // Timer stuff
+    timer = timerBegin(0, 80, true);
+    timerAttachInterrupt(timer, &onTimer, true);
+    timerAlarmWrite(timer, 100000, true);
+    timerAlarmEnable(timer);
 
-  ////////////////////////////////////////////////
-  // Ext Int stuff
-  pinMode(ExtSwitch1.PIN, INPUT_PULLUP);
-//  attachInterrupt(ExtSwitch1.PIN, isr, FALLING);
+    ////////////////////////////////////////////////
+    // Ext Int stuff
+    pinMode(ExtSwitch1.PIN, INPUT_PULLUP);
+    // attachInterrupt(ExtSwitch1.PIN, isr, FALLING);
 }
 
 
@@ -111,36 +111,36 @@ void setup() {
 void loop() {
 
   
-  if((timedInterruptCounter==0) & (flagAt0000==true)){
-    Serial.printf("Init %u counter at\n", timedInterruptCounter);
+    if((timedInterruptCounter==0) & (flagAt0000==true)){
+        Serial.printf("Init %u counter at\n", timedInterruptCounter);
     
-    attachInterrupt(ExtSwitch1.PIN, isr, FALLING);
+        attachInterrupt(ExtSwitch1.PIN, isr, FALLING);
     
-    flagAt0000 = false;
-  }
+        flagAt0000 = false;
+    }
 
 
-  if((timedInterruptCounter==20) & (flagAt0020==true)){
-    detachInterrupt(ExtSwitch1.PIN);
+    if((timedInterruptCounter==20) & (flagAt0020==true)){
+        detachInterrupt(ExtSwitch1.PIN);
     
-    Serial.printf("Two seconds interrupt %u counter at %u\n", timedInterruptCounter, ExtSwitch1.extIntCounter);
+        Serial.printf("Two seconds interrupt %u counter at %u\n", timedInterruptCounter, ExtSwitch1.extIntCounter);
     
-    ExtSwitch1.extIntCounter = 0;
+        ExtSwitch1.extIntCounter = 0;
 
-    flagAt0020 = false;
-  }
+        flagAt0020 = false;
+    }
 
 
-  if(timedInterruptCounter==200){
-    Serial.printf("End %u counter at\n", timedInterruptCounter);  
+    if(timedInterruptCounter==200){
+        Serial.printf("End %u counter at\n", timedInterruptCounter);  
   
-    portENTER_CRITICAL_ISR(&timerMux);
-    timedInterruptCounter = 0;
-    portEXIT_CRITICAL_ISR(&timerMux);
+        portENTER_CRITICAL_ISR(&timerMux);
+        timedInterruptCounter = 0;
+        portEXIT_CRITICAL_ISR(&timerMux);
 
-    flagAt0000 = true;
-    flagAt0020 = true;
-  }
-//   Serial.printf("%s\n",getTimeStamp());
+        flagAt0000 = true;
+        flagAt0020 = true;
+    }
+    // Serial.printf("%s\n",getTimeStamp());
 
 }
