e575ca1cead5fe7b81a1ac46347c1b511b6e2521
Pablo Gianni <giannipablo@gmail.com>
2021-10-02 20:50:36 -0300

    Agregue otro endpoint para leer la base de datos.

--- api/main.py
+++ api/main.py
@@ -46,6 +46,11 @@ class RemoteData(BaseModel):
     payload : Optional[Payload] = None
 
 
+class RequiredData(BaseModel):
+    uuid : UUID
+    dev_name : str
+
+
 
 #####################################
 # Endpoit to push/insert elements to the DB
@@ -71,3 +76,24 @@ async def push(remote_data:RemoteData):
         return {"status" : "FAIL"}
 
 
+#####################################
+# Endpoit to gather data
+@app.post("/pull")
+async def push(required_data:RequiredData):
+    # Guet the logger
+    logger = logging.getLogger('main.pull')
+    try:
+        # Conect to the database
+        uri = f'mongodb://{Config.DB_USER}:{Config.DB_PASS}@{Config.DB_URL}:{Config.DB_PORT}/'
+
+        client = pymongo.MongoClient(uri)
+        
+        pull = client[Config.DB_DATABASE][Config.DB_COLLECTION].find(required_data.dict()).sort([('time_stamp', pymongo.DESCENDING)]).limit(1)
+        
+        logger.info('Data read completed')
+
+        return {"status" : "OK", 'payload' : pull[0]['payload']}
+
+    except Exception as e:
+        logger.error(f'Exception {e} with data: {required_data}') 
+        return {"status" : "FAIL"}
