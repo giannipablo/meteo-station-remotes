9f40672784c88ff23ade6b95a896cf7c7290fe8c
Pablo Gianni <giannipablo@gmail.com>
2021-10-02 21:26:55 -0300

    Modificaciones para arreglar bugs. Now working

--- msbot/main.py
+++ msbot/main.py
@@ -5,24 +5,32 @@ import requests
 from tabulate import tabulate
 
 
-MS_API_KEY = os.getenv('MS_API_KEY')
+API_KEY = os.getenv('MS_API_KEY')
+URL = os.getenv('MS_URL', 'http://127.0.0.1:8000/pull')
 
-bot = telebot.TeleBot(MS_API_KEY)
+bot = telebot.TeleBot(API_KEY)
 
-URL = 'http://homemeteostation.duckdns.org/current'
+# URL = 'http://homemeteostation.duckdns.org/current'
 
-vars = {'temperature': 'Temp', 'humidity': 'Humedad', 'wind_speed': 'Viento'}
+vars = {'temperature': 'Temp', 'humidity': 'Humedad', 'wind_speed': 'Viento', 'preasure': 'Presion'}
+
+qry =  {
+    "uuid" : "640f9d98660d41d7bbe4edf9887de873",
+    "dev_name" : "Ricardo 1"
+}
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['reporte'])
 def send_meteovars(message):
 
-    req = requests.get(URL).json()
-    
+
+
+    req = requests.post(URL, json=qry).json()
+
     valores = []
-    for k,v in vars.iteritems():
+    for k,v in vars.items():
         valores.append([v, req['payload'][k]])
-    valores = tabulate(valores, headers=['Variable', 'Valor'])
+    valores = tabulate(valores, headers=['Variable    ', 'Valor'])
     
     bot.send_message(message.chat.id, valores)
 
